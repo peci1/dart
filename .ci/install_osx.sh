c8caaa12b1c035cf56076c588385aca3878b7a11
Jeongseok Lee <jeoslee@amazon.com>
2020-10-17 14:01:30 -0700

    Test if nlopt/ipopt issue is persistent

--- .ci/install_osx.sh
+++ .ci/install_osx.sh
@@ -5,16 +5,15 @@ brew update > /dev/null
 brew bundle || brew bundle
 brew install open-scene-graph --HEAD  # install master branch until 3.7.0 is released
 
-# pagmo2: build from source until https://github.com/esa/pagmo2/issues/445 is resolved
+# pagmo2: build from source until v2.16.0 is available on Homebrew
 brew install tbb
 git clone https://github.com/esa/pagmo2.git -b 'v2.15.0' --single-branch --depth 1 &&
   cd pagmo2 && mkdir build && cd build &&
   cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DPAGMO_WITH_EIGEN3=ON &&
-  # NLopt and IPopt supports are disabled for https://github.com/esa/pagmo2/issues/445
-  # -DPAGMO_WITH_NLOPT=ON \
-  # -DPAGMO_WITH_IPOPT=ON \
+    -DPAGMO_WITH_NLOPT=ON \
+    -DPAGMO_WITH_IPOPT=ON \
   make -j$(sysctl -n hw.logicalcpu) &&
   make install
 
